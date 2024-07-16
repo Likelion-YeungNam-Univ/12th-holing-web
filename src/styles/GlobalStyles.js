46f116320307e5a4c98a979a96674dda3c0242ca
immms <csi9926@gmail.com>
2024-07-16 22:15:19 +0900

    design: 전역스타일링 제작

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+* {
+  text-decoration: none;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+// TODO : 모바일 화면 크기 & 대표 색상 작성 예정
+
+
+
+`;
+
+export default GlobalStyles;
