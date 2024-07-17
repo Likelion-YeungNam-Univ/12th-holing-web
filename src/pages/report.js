6f9cb21b02decbc4836a40efcbe006e4b57b41e1
immms <csi9926@gmail.com>
2024-07-18 02:24:42 +0900

    design: slide 컴포넌트 제작

--- src/pages/report.js
+++ src/pages/report.js
@@ -1,22 +1,40 @@
 import LineChart from '../components/report/LineChart';
-import {ReportContainer, Wrapper, GraphWrapper, Title} from '../styles/report/ReportPage-styled';
-
+import Slides from '../components/report/Slides';
+import {ReportContainer, TopWrapper, GraphWrapper, Title, TapWrapper, TapBtnWrapper, TapBtn1, TapBtn2, SlideWrapper,} from '../styles/report/ReportPage-styled';
+  
 function Report() {
   return (
     <ReportContainer>
       {/* 상단 리포트 컴포넌트 */}
-      <Wrapper>
+      <TopWrapper>
+
         <Title>
-          홀링 리포트로 나의 증상을
-          <br />
-          한눈으로 확인하세요
+          홀링 리포트로 나의 증상을<br />한눈으로 확인하세요
         </Title>
 
-        {/* 그래프 컴포넌트 */}
-        <GraphWrapper>
-          <LineChart></LineChart>
-        </GraphWrapper>
-      </Wrapper>
+        {/* 탭 컴포넌트 */}
+        <TapWrapper>
+          {/* 탭버튼 */}
+          <TapBtnWrapper>
+            <TapBtn1>나의 증상분석</TapBtn1>
+            <TapBtn2>짝꿍의 증상분석</TapBtn2>
+          </TapBtnWrapper>
+          {/* 그래프*/}
+          <GraphWrapper>
+            <LineChart></LineChart>
+          </GraphWrapper>
+
+
+          {/* 슬라이드 컴포넌트*/}
+          <SlideWrapper>
+            <Slides></Slides>
+            <Slides></Slides>
+          </SlideWrapper>
+
+        </TapWrapper>
+
+      </TopWrapper>
+
 
       {/* TODO: 하단 테스트 컴포넌트 */}
     </ReportContainer>
