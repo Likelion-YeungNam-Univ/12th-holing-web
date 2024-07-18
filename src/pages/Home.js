8b58e7a60c78941abc7eab4ae42b199aeb97799e
dev-joohee <dev.joohee0916@gmail.com>
2024-07-19 00:14:35 +0900

    사용자와 짝꿍 레이아웃

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MySummary from '../components/home/mySummary/MySummary';
+// import PartnerSummary from '../components/home/partnerSummary/PartnerSummary';
 
 function Home() {
   return (
     <>
       <MySummary />
+      {/* <PartnerSummary /> */}
     </>
   );
 }
