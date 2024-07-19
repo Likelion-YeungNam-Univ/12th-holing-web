31fc8712ba9e74db6178c4cde6dc50830699fab9
dev-joohee <dev.joohee0916@gmail.com>
2024-07-19 21:44:17 +0900

    feat: 영양제 알림

---
+++ src/components/home/medicineNote/AddMedicine.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddMedicine() {
+  return <div>AddMedicine</div>;
+}
+
+export default AddMedicine;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MySummary from '../components/home/mySummary/MySummary';
 import SymptomTest from '../components/home/symptomTest/SymptomTest';
+import MedicineNote from '../components/home/medicineNote/MedicineNote';
 // import PartnerSummary from '../components/home/partnerSummary/PartnerSummary';
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <MySummary />
       {/* <PartnerSummary /> */}
       <SymptomTest />
+      <MedicineNote />
     </>
   );
 }
