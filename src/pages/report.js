aca86a259b54bb42aa385cfe36fb0735c3655e41
immms <csi9926@gmail.com>
2024-07-21 01:14:19 +0900

    design: 리포트 컴포넌트 제작 완료

--- src/pages/report.js
+++ src/pages/report.js
@@ -1,7 +1,35 @@
 import LineChart from 'components/report/LineChart';
-import { ReportContainer, TopWrapper, GraphWrapper, Title, TapWrapper, TapBtnWrapper, TapBtn1, TapBtn2, } from '../styles/report/ReportPage-styled';
-import { GotoTestWrapper, TopTitle, TestIcons, BottomBtn } from 'styles/report/GotoTestPage-styled';
 import SlideAnimation from 'components/report/SlideAnimation';
+import {
+  ReportContainer,
+  TopWrapper,
+  GraphWrapper,
+  Title,
+  TapWrapper,
+  TapBtnWrapper,
+  TapBtn1,
+  TapBtn2
+} from 'styles/report/ReportPage-styled';
+
+import {
+  GotoTestWrapper,
+  TopTitle,
+  IconsWrapper,
+  LeftIcon,
+  CenterIcon,
+  RightIcon,
+  LeftTop,
+  LeftBottom,
+  CenterTop,
+  CenterBottom,
+  RightTop,
+  RightBottom,
+  BottomBtn,
+  LeftIconImage,
+  CenterIconImage,
+  RightIconImage
+} from 'styles/report/GotoTestPage-styled';
+
 
 function Report() {
   return (
@@ -10,10 +38,12 @@ function Report() {
       <TopWrapper>
         {/* TODO : 데이터는 추후 props로 구현 예정 - 임시로 text입력 상태*/}
         <Title>
-          홀링 리포트로 나의 증상을<br />한눈으로 확인하세요
+          홀링 리포트로 나의 증상을
+          <br />
+          한눈으로 확인하세요
         </Title>
 
-        {/* 탭 컴포넌트 */}
+        {/* TODO : 탭 컴포넌트 분리 */}
         <TapWrapper>
           {/* 탭버튼 */}
           <TapBtnWrapper>
@@ -22,28 +52,48 @@ function Report() {
           </TapBtnWrapper>
           {/* 그래프*/}
           <GraphWrapper>
-            <LineChart></LineChart>
+            <LineChart />
           </GraphWrapper>
 
-
           {/* 슬라이드*/}
-          <SlideAnimation></SlideAnimation>
-        </TapWrapper> 
+          <SlideAnimation />
+        </TapWrapper>
       </TopWrapper>
 
-
-      {/* TODO: 하단 테스트 컴포넌트 */}
+      {/* TODO: 하단 테스트 컴포넌트 분리 */}
       <GotoTestWrapper>
         <TopTitle>
-          <h2>이번주 나의 증상을 <p>테스트</p>로 기록해보세요</h2>
+          <h2>
+            이번주 나의 증상을<br />테스트로 기록해보세요
+          </h2>
         </TopTitle>
-        <TestIcons>icons</TestIcons>
+
+        {/* TODO: props로 data 전달 예정 */}
+        <IconsWrapper>
+          <LeftIcon>
+            <LeftIconImage />
+            <LeftTop>N주차 테스트</LeftTop>
+            <LeftBottom>240608</LeftBottom>
+          </LeftIcon>
+
+          <CenterIcon>
+            <CenterIconImage />
+            <CenterTop>N주차 테스트</CenterTop>
+            <CenterBottom>D-DAY</CenterBottom>
+          </CenterIcon>
+
+          <RightIcon>
+            <RightIconImage />
+            <RightTop>N주차 테스트</RightTop>
+            <RightBottom>D-007</RightBottom>
+          </RightIcon>
+        </IconsWrapper>
+
         <BottomBtn>
-          <btn>나의 증상 테스트하기</btn>
+          <button>나의 증상 테스트하기</button>
           <p>매주 테스트를 통해 리포트를 제공받아요</p>
         </BottomBtn>
       </GotoTestWrapper>
-
     </ReportContainer>
   );
 }
