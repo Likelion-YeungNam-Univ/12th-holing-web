70ebbb3339d613dfd5a949ad87f2a1a3b5d37f6d
immms <csi9926@gmail.com>
2024-07-21 16:40:41 +0900

    design: Swiper.js navigation 화살표 제거

--- src/components/report/SlideAnimation.js
+++ src/components/report/SlideAnimation.js
@@ -5,19 +5,15 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Swiper 스타일 Import
 import 'swiper/css';
-import 'swiper/css/navigation';
 
 // 필요모듈 import
-import { Navigation } from 'swiper/modules';
 import CurrentSlide from './CurrentSlide';
 import PastSlide from './PastSlide';
 
 export default function SlideAnimation() {
   return (
     <SwiperWrapper>
-      <Swiper navigation={true} 
-              modules={[Navigation]}
-              slidesPerView= {2} // 화면에 보여질 slide 갯수
+      <Swiper slidesPerView= {2} // 화면에 보여질 slide 갯수
               slidesOffsetAfter={40} // 오른쪽 끝에 여백 추가
       >
         {/* TODO : 추후 props로 Slides컴포넌트 연결 예정 */}
