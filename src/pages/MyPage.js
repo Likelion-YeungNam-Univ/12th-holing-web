f833a548bba91b19b29c9182676af9634acd1b93
minseo <choms1357@gmail.com>
2024-07-22 01:50:11 +0900

    design: 마이페이지 디자인

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -9,15 +9,81 @@ import {
   Credit,
   CreditBox,
   CreditItemBox,
+  CreditItemBoxRow,
   CreditItemTitle,
   CreditItemDescription,
   CreditItemExchangeBtn,
   CreditItemPrice,
   CreditItem,
-  CreditItemBoxRow,
 } from '../style/MyPage-styled';
 import img_creditItem1 from '../assets/images/credit_item1.png';
 import img_creditItem2 from '../assets/images/credit_item2.png';
+import img_creditItem3 from '../assets/images/credit_item3.png';
+import img_creditItem4 from '../assets/images/credit_item4.png';
+import img_creditItem5 from '../assets/images/credit_item5.png';
+import img_creditItem6 from '../assets/images/credit_item6.png';
+import img_creditItem7 from '../assets/images/credit_item7.png';
+import img_creditItem8 from '../assets/images/credit_item8.png';
+
+// 아이템 데이터 배열
+const creditItems = [
+  [
+    {
+      img: img_creditItem1,
+      title: '괄사&마사지오일',
+      description: '[랠리] 콤부차(10p) 4종 택 1',
+      price: '5500 크레딧',
+    },
+    {
+      img: img_creditItem2,
+      title: '콤부차',
+      description: '[메디테라피,벨레다] 속살 괄사&아니카 마사지',
+      price: '3500 크레딧',
+    },
+  ],
+  [
+    {
+      img: img_creditItem3,
+      title: '요가링',
+      description: '[센시안] 요가링(1p)',
+      price: '3500 크레딧',
+    },
+    {
+      img: img_creditItem4,
+      title: '마시지 도구',
+      description: '[마이롤러] 마이 마사지테라피볼 피넛형',
+      price: '6500 크레딧',
+    },
+  ],
+  [
+    {
+      img: img_creditItem5,
+      title: '마사지 도구',
+      description: '[마이웨잇] 지압버섯',
+      price: '6500 크레딧',
+    },
+    {
+      img: img_creditItem6,
+      title: '홈트밴드',
+      description: '[마이웨잇] 마이 홈트밴드 세트',
+      price: '18500 크레딧',
+    },
+  ],
+  [
+    {
+      img: img_creditItem7,
+      title: '마사지 오일',
+      description: '[메디테라피] 인더 포레스트 히노키 바디오션',
+      price: '12500 크레딧',
+    },
+    {
+      img: img_creditItem8,
+      title: '수면안대',
+      description: '[바른생각] 눈편한생각(5p)',
+      price: '3500 크레딧',
+    },
+  ],
+];
 
 function MyPage() {
   return (
@@ -33,54 +99,21 @@ function MyPage() {
           <Credit>크레딧</Credit>
         </CreditBox>
       </CreditWrapper>
-      <CreditItemBoxRow>
-        <CreditItemBox>
-          <CreditItem>
-            <img src={img_creditItem1}></img>
-          </CreditItem>
-          <CreditItemTitle>괄사&마사지오일</CreditItemTitle>
-          <CreditItemDescription>
-            [랠리] 콤부차(10p) 4종 택 1
-          </CreditItemDescription>
-          <CreditItemPrice>5500 크레딧</CreditItemPrice>
-          <CreditItemExchangeBtn>교환하기</CreditItemExchangeBtn>
-        </CreditItemBox>
-        <CreditItemBox>
-          <CreditItem>
-            <img src={img_creditItem2}></img>
-          </CreditItem>
-          <CreditItemTitle>콤부차</CreditItemTitle>
-          <CreditItemDescription>
-            [메디테라피,벨레다] 속살 괄사&아니카 마사지
-          </CreditItemDescription>
-          <CreditItemPrice>3500 크레딧</CreditItemPrice>
-          <CreditItemExchangeBtn>교환하기</CreditItemExchangeBtn>
-        </CreditItemBox>
-      </CreditItemBoxRow>
-      <CreditItemBoxRow>
-        <CreditItemBox>
-          <CreditItem>
-            <img src={img_creditItem1}></img>
-          </CreditItem>
-          <CreditItemTitle>괄사&마사지오일</CreditItemTitle>
-          <CreditItemDescription>
-            [랠리] 콤부차(10p) 4종 택 1
-          </CreditItemDescription>
-          <CreditItemPrice>5500 크레딧</CreditItemPrice>
-          <CreditItemExchangeBtn>교환하기</CreditItemExchangeBtn>
-        </CreditItemBox>
-        <CreditItemBox>
-          <CreditItem>
-            <img src={img_creditItem2}></img>
-          </CreditItem>
-          <CreditItemTitle>콤부차</CreditItemTitle>
-          <CreditItemDescription>
-            [메디테라피,벨레다] 속살 괄사&아니카 마사지
-          </CreditItemDescription>
-          <CreditItemPrice>3500 크레딧</CreditItemPrice>
-          <CreditItemExchangeBtn>교환하기</CreditItemExchangeBtn>
-        </CreditItemBox>
-      </CreditItemBoxRow>
+      {creditItems.map((row, rowIndex) => (
+        <CreditItemBoxRow key={rowIndex}>
+          {row.map((item, itemIndex) => (
+            <CreditItemBox key={itemIndex}>
+              <CreditItem>
+                <img src={item.img} alt={item.title} />
+              </CreditItem>
+              <CreditItemTitle>{item.title}</CreditItemTitle>
+              <CreditItemDescription>{item.description}</CreditItemDescription>
+              <CreditItemPrice>{item.price}</CreditItemPrice>
+              <CreditItemExchangeBtn>교환하기</CreditItemExchangeBtn>
+            </CreditItemBox>
+          ))}
+        </CreditItemBoxRow>
+      ))}
     </>
   );
 }
