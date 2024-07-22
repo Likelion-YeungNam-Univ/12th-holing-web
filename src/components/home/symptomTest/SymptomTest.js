393c4ff033b7f30a8fb71c2c05289a4a92e17297
dev-joohee <dev.joohee0916@gmail.com>
2024-07-23 03:29:58 +0900

    Fix: 절대 경로 변경

--- src/components/home/symptomTest/SymptomTest.js
+++ src/components/home/symptomTest/SymptomTest.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SymptomTestBtn } from '../../../styles/home/SympomTest-styled';
+import { SymptomTestBtn } from 'styles/home/SympomTest-styled';
 import { Link } from 'react-router-dom';
 
 function SymptomTest() {
