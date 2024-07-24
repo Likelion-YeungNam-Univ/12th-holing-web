9325025084eaa66775f6844f94ec7e7125cddf67
dev-joohee <dev.joohee0916@gmail.com>
2024-07-24 17:49:06 +0900

    절대 경로 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import MySummary from '../components/home/mySummary/MySummary';
-import SymptomTest from '../components/home/symptomTest/SymptomTest';
-import MedicineNote from '../components/home/medicineNote/MedicineNote';
-// import PartnerSummary from '../components/home/partnerSummary/PartnerSummary';
-import { Wrapper } from '../styles/home/Home-styled';
-import Mission from '../components/home/mission/Mission';
+import MySummary from 'components/home/mySummary/MySummary';
+import SymptomTest from 'components/home/symptomTest/SymptomTest';
+import MedicineNote from 'components/home/medicineNote/MedicineNote';
+import { Wrapper } from 'styles/home/Home-styled';
+import Mission from 'components/home/mission/Mission';
 
 function Home() {
   return (
