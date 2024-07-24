eeed0b06f24a1f8ee414e8be31a935bd3a5a0eac
dev-joohee <dev.joohee0916@gmail.com>
2024-07-24 18:19:40 +0900

    styled 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Report from 'pages/report';
 import GlobalStyles from 'styles/GlobalStyles';
+import Home from 'pages/Home';
 import Frame from 'components/comonents/Frame';
 import MyFullCalendar from 'pages/MyFullCalendar';
 import MyPage from 'pages/MyPage';
