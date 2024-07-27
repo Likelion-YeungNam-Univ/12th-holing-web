e2a2ac58eb872476137758102f2aa2b28f3951aa
dev-joohee <dev.joohee0916@gmail.com>
2024-07-28 00:30:04 +0900

    불필요 코드 삭제

--- src/pages/LogUserInfo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import KakaoLogUserInfo from 'components/login/KakaoLogUserInfo';
-const LogUserInfo = () => {
-  return <KakaoLogUserInfo />;
-};
-
-export default LogUserInfo;
--- src/pages/Logout.js
+++
@@ -1,8 +0,0 @@
-import KakaoLogout from '../components/login/KakaoLogout';
-import React from 'react';
-
-const Login = () => {
-  return <KakaoLogout />;
-};
-
-export default Logout;
