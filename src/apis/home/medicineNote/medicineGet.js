507272e6637ee05e3c367893b63535d92300f8ad
dev-joohee <dev.joohee0916@gmail.com>
2024-08-01 20:13:23 +0900

    feat: 영양제목록조회

--- src/apis/home/medicineNote/medicineGet.js
+++ src/apis/home/medicineNote/medicineGet.js
@@ -3,17 +3,16 @@ import axios from 'axios';
 const apiUrl = process.env.REACT_APP_API_URL;
 const authToken = process.env.REACT_APP_API_AUTH_TOKEN;
 
-const postMedicine = (medicines) => {
-  const endPoint = '/user/medicines';
+const getMedicines = () => {
+  const endPoint = `/user/medicines`;
   const url = `${apiUrl}${endPoint}`;
 
-  return axios.post(url, scheduleData, {
+  return axios.get(url, {
     headers: {
       Accept: '*/*',
-      'Content-Type': 'application/json',
       Authorization: authToken,
     },
   });
 };
 
-export { postMedicine };
+export { getMedicines };
