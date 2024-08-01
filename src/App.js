b42a59412dfbc79cb04e646e1dc7c91d68062ada
dev-joohee <dev.joohee0916@gmail.com>
2024-08-01 22:42:34 +0900

    fix: 불필요 코드 수정

--- src/App.js
+++ src/App.js
@@ -18,45 +18,42 @@ import TestFrame from 'components/comonents/frame/TestFrame';
 import IntroFrame from 'components/comonents/frame/IntroFrame';
 import Intro from 'pages/intro';
 
-import { CookiesProvider } from 'react-cookie';
-
 // import Kakaologin from 'pages/Kakaologin';
 
 import UserInfo from 'pages/UserInfo';
-import FeSelfTest from 'pages/SelfTest/FeSelfTest';
-import SelfTest from 'pages/SelfTest/SelfTest';
-import SelfTest2 from 'pages/SelfTest/SelfTest2';
-import SelfTest3 from 'pages/SelfTest/SelfTest3';
-import SelfTest4 from 'pages/SelfTest/SelfTest4';
-import SelfTest5 from 'pages/SelfTest/SelfTest5';
-import SelfTest6 from 'pages/SelfTest/SelfTest6';
-import SelfTest7 from 'pages/SelfTest/SelfTest7';
-import SelfTest8 from 'pages/SelfTest/SelfTest8';
-import SelfTest9 from 'pages/SelfTest/SelfTest9';
-import SelfTest10 from 'pages/SelfTest/SelfTest10';
+import FeSelfTest from 'pages/selfTest/FeSelfTest';
+import SelfTest from 'pages/selfTest/SelfTest';
+import SelfTest2 from 'pages/selfTest/SelfTest2';
+import SelfTest3 from 'pages/selfTest/SelfTest3';
+import SelfTest4 from 'pages/selfTest/SelfTest4';
+import SelfTest5 from 'pages/selfTest/SelfTest5';
+import SelfTest6 from 'pages/selfTest/SelfTest6';
+import SelfTest7 from 'pages/selfTest/SelfTest7';
+import SelfTest8 from 'pages/selfTest/SelfTest8';
+import SelfTest9 from 'pages/selfTest/SelfTest9';
+import SelfTest10 from 'pages/selfTest/SelfTest10';
 import Start from 'pages/Start';
 
 function App() {
   return (
-    <CookiesProvider>
-      <React.Fragment>
-        <GlobalStyles /> {/* 전역스타일링 */}
-        <BrowserRouter>
-          <Routes>
-            {/* 기본 화면프레임 */}
-            <Route path="/" element={<Frame />}>
-              {' '}
-              <Route index element={<Home />} />
-              <Route path="/report" element={<Report />} />
-              <Route path="/calendar" element={<MyFullCalendar />} />
-              <Route path="/my" element={<MyPage />} />
-              <Route path="/reportDetail" element={<ReportDetail />} />
-              <Route path="/account-info" element={<AccountInfo />} />
-            </Route>
     <React.Fragment>
       <GlobalStyles /> {/* 전역스타일링 */}
       <BrowserRouter>
         <Routes>
+          {/* 기본 화면프레임 */}
+          <Route path="/" element={<Frame />}>
+            {' '}
+            <Route index element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="/calendar" element={<MyFullCalendar />} />
+            <Route path="/my" element={<MyPage />} />
+            <Route path="/reportDetail" element={<ReportDetail />} />
+            <Route path="/account-info" element={<AccountInfo />} />
+          </Route>
+          {/* <React.Fragment> */}
+          {/* <GlobalStyles /> {/* 전역스타일링 */}
+          {/* <BrowserRouter>
+        <Routes>  */}
           <Route path="/start" element={<Start />} />
           {/* 기본 화면프레임 */}
           <Route element={<Frame />}>
@@ -81,25 +78,24 @@ function App() {
             <Route path="/selftest10" element={<SelfTest10 />} />
           </Route>
 
-            {/* 테스트 화면프레임 */}
-            <Route path="/" element={<TestFrame />}>
-              <Route path="/symptomTest" element={<SymptomTest />} />
+          {/* 테스트 화면프레임 */}
+          <Route path="/" element={<TestFrame />}>
+            <Route path="/symptomTest" element={<SymptomTest />} />
 
-              <Route path="/login" element={<Login />} />
-              {/*서비스 소개 후 로그인 버튼 있는 페이지*/}
+            <Route path="/login" element={<Login />} />
+            {/*서비스 소개 후 로그인 버튼 있는 페이지*/}
 
-              {/*짝꿍과 공유하기 페이지*/}
-            </Route>
+            {/*짝꿍과 공유하기 페이지*/}
+          </Route>
 
-            {/* 로고 가운데 화면프레임 */}
-            <Route path="/" element={<IntroFrame />}>
-              <Route path="/kakaologin" element={<ShareUrl />} />
-              <Route path="/intro" element={<Intro />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </React.Fragment>
-    </CookiesProvider>
+          {/* 로고 가운데 화면프레임 */}
+          <Route path="/" element={<IntroFrame />}>
+            <Route path="/kakaologin" element={<ShareUrl />} />
+            <Route path="/intro" element={<Intro />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 export default App;
