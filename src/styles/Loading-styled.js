6c87b43bc361858d29bca9897e6267b75cffb9ca
minseo <choms5235@naver.com>
2024-08-06 16:53:33 +0900

    feat: 로딩 화면 제작

---
+++ src/styles/Loading-styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: rgba(0, 0, 0, 0.6);
+  width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
